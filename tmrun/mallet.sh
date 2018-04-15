c9192b4eb9624734a1cda8a49b7ca8b0d7d76a43
Brendan O'Connor <brenocon@gmail.com>
2018-04-15 15:48:01 -0400

    tmrun

--- tmrun/mallet.sh
+++ tmrun/mallet.sh
@@ -3,14 +3,17 @@ set -eux
 
 indir="$1"   # format: one text file for every doc
 outname=$2
+ntopics=$3
 # outname="${indir:a:t}"
 echo "input $indir => output $outname"
 
 MALLET=$HOME/sw/ml/mallet-2.0.7/bin/mallet
 
-$MALLET import-dir --input "$indir" --output "${outname}.mallet_import"  --token-regex '\S+' --keep-sequence --remove-stopwords
+$MALLET import-dir --input "$indir" --output "${outname}.mallet_import"  \
+    --token-regex '[\p{L}\p{N}_]+' --keep-sequence --remove-stopwords
+# $MALLET import-dir --input "$indir" --output "${outname}.mallet_import"  --keep-sequence --remove-stopwords
 
-$MALLET train-topics --input "${outname}.mallet_import" --num-topics 10 --output-state "${outname}.tmstate.gz" --optimize-interval 10
+$MALLET train-topics --input "${outname}.mallet_import" --num-topics $ntopics --output-state "${outname}.tmstate.gz" --optimize-interval 10
 
 # view phrase-y topics
 # zcat "${outname}.tmstate.gz" | python post.py
